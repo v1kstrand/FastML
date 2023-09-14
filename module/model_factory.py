5b852d0f68b6175dc04737d20f2f7d34d6b452f0
David <david.vikstrand@gmail.com>
2023-09-14 14:30:33 +0200

    Update model_factory.py

--- module/model_factory.py
+++ module/model_factory.py
@@ -83,8 +83,8 @@ class ModelFactory:
             SVR(): {"C": [1, 10, 100], "gamma": ["auto"]},
             NeuralNetRegressor(ANNRegressor): {
                 "module__num_features": [num_features],
-                "max_epochs": [50],
-                "lr": [0.01],
+                "max_epochs": [60],
+                "lr": [0.005],
                 "optimizer": [torch.optim.Adam],
                 "verbose": [0],
                 "device": ["cuda" if torch.cuda.is_available() else "cpu"],
@@ -124,8 +124,8 @@ class ModelFactory:
             NeuralNetClassifier(ANNClassifier): {
                 "module__num_classes": [num_classes],
                 "module__num_features": [num_features],
-                "max_epochs": [50],
-                "lr": [0.01],
+                "max_epochs": [60],
+                "lr": [0.005],
                 "optimizer": [torch.optim.Adam],
                 "verbose": [0],
                 "device": ["cuda" if torch.cuda.is_available() else "cpu"],
